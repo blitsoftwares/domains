c8eb4ec4f9112a58ed5803a2fa7549ca9d25be8e
Lucas R.Pasquetto <lucas@blitsoft.com.br>
2017-12-07 17:19:59 -0300

    Lag pt-br

---
+++ src/Lang/pt-br/domains.php
@@ -0,0 +1,25 @@
+<?php
+
+return [
+
+    'domains' => 'Dominios',
+    'new-register' => 'Novo registro',
+    'route-back' => 'Voltar',
+    'action' => 'Ações',
+    'submit' => 'Salvar',
+    'edit' => 'Editar',
+    'delete' => 'Deletar',
+
+    'fields' => [
+        'slug' => 'Slug',
+        'name' => 'Nome',
+        'active' => 'Ativo',
+    ],
+
+    'table-header' => [
+        'slug' => 'Slug',
+        'name' => 'Nome',
+        'active' => 'Ativo',
+    ],
+
+];
